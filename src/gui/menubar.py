99b7521cc4b478c0c8b91b1952125b6d3beedb16
Pavel Tisnovsky <ptisnovs@redhat.com>
2017-09-22 09:25:58 +0200

    Updated menubar to use new icons

--- src/gui/menubar.py
+++ src/gui/menubar.py
@@ -11,8 +11,16 @@
 #
 
 import tkinter
-import icons.exit
-import icons.open
+
+import icons.application_exit
+import icons.file_open
+import icons.file_save
+import icons.file_import
+import icons.zoom_in
+import icons.zoom_out
+import icons.zoom_original
+import icons.help_about
+import icons.help_faq
 
 
 class Menubar(tkinter.Menu):
@@ -20,12 +28,21 @@ class Menubar(tkinter.Menu):
     def __init__(self, parent):
         super().__init__(tearoff=0)
 
-        self.exit_icon = tkinter.PhotoImage(data=icons.exit.icon)
-        self.open_icon = tkinter.PhotoImage(data=icons.open.icon)
+        self.exit_icon = tkinter.PhotoImage(data=icons.application_exit.icon)
+        self.file_import_icon = tkinter.PhotoImage(data=icons.file_import.icon)
+        self.file_open_icon = tkinter.PhotoImage(data=icons.file_open.icon)
+        self.zoom_in_icon = tkinter.PhotoImage(data=icons.zoom_in.icon)
+        self.zoom_out_icon = tkinter.PhotoImage(data=icons.zoom_out.icon)
+        self.zoom_original_icon = tkinter.PhotoImage(
+            data=icons.zoom_original.icon)
+        self.help_about_icon = tkinter.PhotoImage(data=icons.help_about.icon)
+        self.help_faq_icon = tkinter.PhotoImage(data=icons.help_faq.icon)
 
         filemenu = tkinter.Menu(self, tearoff=0)
-        filemenu.add_command(label="Importovat nový výkres")
-        filemenu.add_command(label="Otevřít výkres", image=self.open_icon,
+        filemenu.add_command(label="Importovat nový výkres",
+                             image=self.file_import_icon,
+                             compound="left", underline=0)
+        filemenu.add_command(label="Otevřít výkres", image=self.file_open_icon,
                              compound="left", underline=0)
         filemenu.add_separator()
         filemenu.add_command(label="Konec", image=self.exit_icon,
@@ -33,13 +50,23 @@ class Menubar(tkinter.Menu):
                              command=parent.quit)
 
         edit = tkinter.Menu(self, tearoff=0)
+
         view = tkinter.Menu(self, tearoff=0)
+        view.add_command(label="Zvětšit", image=self.zoom_in_icon,
+                         compound="left", underline=1)
+        view.add_command(label="Zmenšit", image=self.zoom_out_icon,
+                         compound="left", underline=1)
+        view.add_command(label="1:1", image=self.zoom_original_icon,
+                         compound="left", underline=0)
 
         tools = tkinter.Menu(self, tearoff=0)
         tools.add_command(label="Seznam místností")
 
         helpmenu = tkinter.Menu(self, tearoff=0)
-        helpmenu.add_command(label="O programu")
+        helpmenu.add_command(label="Nápověda", image=self.help_faq_icon,
+                             compound="left", underline=0)
+        helpmenu.add_command(label="O programu", image=self.help_about_icon,
+                             compound="left", underline=0)
 
         self.add_cascade(label="Soubor", menu=filemenu)
         self.add_cascade(label="Úpravy", menu=edit)
