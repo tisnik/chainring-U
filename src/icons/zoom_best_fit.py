b63f4d3f1cc938fba2816f47fab377ba67afcb3f
Pavel Tisnovsky <ptisnovs@redhat.com>
2017-09-22 09:26:52 +0200

    New icon: zoom best fit

---
+++ src/icons/zoom_best_fit.py
@@ -0,0 +1,18 @@
+# This icon was converted from the Oxygen Icon Theme
+# Please see copyright for further informations
+
+icon="""
+iVBORw0KGgoAAAANSUhEUgAAABYAAAAWCAMAAADzapwJAAABL1BMVEV4eHgAAAAAAAAAAAAAAAAA
+AAAAAAAAAAAAAAAAAAAAAAC/v7MAAAAAAAAAAAAAAAAAAAA+PjYAAAA5OTIAAAAAAAAAAAD7+/sa
+GhoVFRX8/Pz6+vr8/Py8v7m+wbtwcW5NTktMTUr///++wLtoaWZ3eXS9wLi9wLlnaGX///////+X
+mJXNzspaXVj///+5uri8vbvn6Obp6ujq6+mys7H///9YWlZpa2ZvcWx1d3J6fXd7fniBhH+DhYCL
+joiUl5GanZedn5mgo52lqKKmqaOtsKmusaq2ubK6vbbe3tzg4N7h4uDj4+Ho6Obo6Ojp6ejp6ujq
+6unq6+rt7evw8O/19fT19fX19vX29vb29/b39/f4+Pf4+Pj5+fn6+vn6+vr8/Pv8/Pz9/f3+/v7/
+//8DCP3AAAAANnRSTlMAAQIDBAUGBwoLDRQVFhgdHyEkJCUoKTpGVmJsoaioqdTW2uzu7u7u8PDx
+9ff4+Pr6+/v8/f0rjvqjAAABI0lEQVR42pXMW1OCUBSGYcDE0spOdlYrUyu1sqzAE4qkph2oEHW7
+1YD1/39DmxUx02XPxZpv3ovF/Vsknun6MvGIV1NFC3xfxdRPT9w6tuWznZsE5qw9zz0w06l7c3M7
+i7kLVDoEkCiV4PJAotD1MpFBlmVC2LmTiZc7QCpQIS4c0MHchkm1Wp18MjigjbnlUNcLg8NpYW5a
+z39YTczKrKcoSu+JwTFTMNfH/QY0+i4c4zrmmqmroKqqrrNzrupmDXN5YGj7F6AZhgYnO5oxKGNO
+f5j5R2Y0cm/efE+zyPPJ6yGd+ujwKsnznBDaPCu9kV+j19LpuihwC+Ho3nHh3lc42l1dDHCCGF5Z
+24pte2Ib0eWloMCeC4GgKIY8ohgMCDz3DWQZXPkRfUO0AAAAAElFTkSuQmCC
+"""
