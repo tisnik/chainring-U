cc0514c93f21da8e010a2b49cbb112b3d833d3ee
Pavel Tisnovsky <ptisnovs@redhat.com>
2017-09-25 09:48:53 +0200

    Entity: arc

---
+++ src/entities/arc.py
@@ -0,0 +1,7 @@
+class Arc:
+    def __init__(self, x, y, radius, angle1, angle2):
+        self.x = x
+        self.y = y
+        self.radius = radius
+        self.angle1 = angle1
+        self.angle2 = angle2
