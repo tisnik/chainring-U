c02dc956b56185c36d09585fdcda1e6546973e27
Pavel Tisnovsky <ptisnovs@redhat.com>
2017-09-26 10:22:55 +0200

    Added license into the module line.py

--- src/entities/line.py
+++ src/entities/line.py
@@ -1,6 +1,21 @@
+#
+#  (C) Copyright 2017  Pavel Tisnovsky
+#
+#  All rights reserved. This program and the accompanying materials
+#  are made available under the terms of the Eclipse Public License v1.0
+#  which accompanies this distribution, and is available at
+#  http://www.eclipse.org/legal/epl-v10.html
+#
+#  Contributors:
+#      Pavel Tisnovsky
+#
+
 class Line:
     def __init__(self, x1, y1, x2, y2):
         self.x1 = x1
         self.x1 = x1
         self.y2 = y2
         self.y2 = y2
+
+    def str(self):
+        return "L"
