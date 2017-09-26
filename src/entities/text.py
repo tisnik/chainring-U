1d033eaa15762a117817df48760ac506b3382a58
Pavel Tisnovsky <ptisnovs@redhat.com>
2017-09-26 10:24:25 +0200

    Added license into the module text.py

--- src/entities/text.py
+++ src/entities/text.py
@@ -1,5 +1,20 @@
+#
+#  (C) Copyright 2017  Pavel Tisnovsky
+#
+#  All rights reserved. This program and the accompanying materials
+#  are made available under the terms of the Eclipse Public License v1.0
+#  which accompanies this distribution, and is available at
+#  http://www.eclipse.org/legal/epl-v10.html
+#
+#  Contributors:
+#      Pavel Tisnovsky
+#
+
 class Text:
     def __init__(self, x, y, text):
         self.x = x
         self.y = y
         self.text = text
+
+    def str(self):
+        return "T"
