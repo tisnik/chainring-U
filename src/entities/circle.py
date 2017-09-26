fc84c0cda3c3f5aabcf7be1a62174b01be7ac469
Pavel Tisnovsky <ptisnovs@redhat.com>
2017-09-26 10:19:24 +0200

    Added license into the module circle.py

--- src/entities/circle.py
+++ src/entities/circle.py
@@ -1,5 +1,20 @@
+#
+#  (C) Copyright 2017  Pavel Tisnovsky
+#
+#  All rights reserved. This program and the accompanying materials
+#  are made available under the terms of the Eclipse Public License v1.0
+#  which accompanies this distribution, and is available at
+#  http://www.eclipse.org/legal/epl-v10.html
+#
+#  Contributors:
+#      Pavel Tisnovsky
+#
+
 class Circle:
     def __init__(self, x, y, radius):
         self.x = x
         self.y = y
         self.radius = radius
+
+    def str(self):
+        return "C"
