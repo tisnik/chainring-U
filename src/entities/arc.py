a6b2f324e762eea6065ebad6f7a94aeeeacd8f4c
Pavel Tisnovsky <ptisnovs@redhat.com>
2017-09-26 10:17:56 +0200

    Added license into the module arc.py

--- src/entities/arc.py
+++ src/entities/arc.py
@@ -1,3 +1,15 @@
+#
+#  (C) Copyright 2017  Pavel Tisnovsky
+#
+#  All rights reserved. This program and the accompanying materials
+#  are made available under the terms of the Eclipse Public License v1.0
+#  which accompanies this distribution, and is available at
+#  http://www.eclipse.org/legal/epl-v10.html
+#
+#  Contributors:
+#      Pavel Tisnovsky
+#
+
 class Arc:
     def __init__(self, x, y, radius, angle1, angle2):
         self.x = x
@@ -5,3 +17,6 @@ class Arc:
         self.radius = radius
         self.angle1 = angle1
         self.angle2 = angle2
+
+    def str(self):
+        return "A"
