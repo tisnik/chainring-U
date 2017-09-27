b09b7f223bb1ed55f88238ac8ad2eb916a368731
Pavel Tisnovsky <ptisnovs@redhat.com>
2017-09-27 09:17:56 +0200

    Icons on the toolbar

--- src/gui/toolbar.py
+++ src/gui/toolbar.py
@@ -12,23 +12,54 @@
 
 import tkinter
 
+import icons.file_open
+import icons.file_save
+import icons.file_import
+
 import icons.zoom_in
 import icons.zoom_out
 import icons.zoom_original
+
+import icons.room_list
 
 
 class Toolbar(tkinter.LabelFrame):
     def __init__(self, parent):
         super().__init__(parent, text="Nástroje", padx=5, pady=5)
 
+        self.file_import_icon = tkinter.PhotoImage(data=icons.file_import.icon)
+        self.file_open_icon = tkinter.PhotoImage(data=icons.file_open.icon)
+        self.file_save_icon = tkinter.PhotoImage(data=icons.file_save.icon)
+
         self.zoom_in_icon = tkinter.PhotoImage(data=icons.zoom_in.icon)
         self.zoom_out_icon = tkinter.PhotoImage(data=icons.zoom_out.icon)
         self.zoom_original_icon = tkinter.PhotoImage(data=icons.zoom_original.icon)
 
-        b1 = tkinter.Button(self, text="Zvětšit", image=self.zoom_in_icon)
-        b2 = tkinter.Button(self, text="Zmenšit", image=self.zoom_out_icon)
-        b3 = tkinter.Button(self, text="1:1", image=self.zoom_original_icon)
+        self.room_list_icon = tkinter.PhotoImage(data=icons.room_list.icon)
 
-        b1.grid(column=1, row=1)
-        b2.grid(column=2, row=1)
-        b3.grid(column=3, row=1)
+        button_import = tkinter.Button(self, text="Import", image=self.file_import_icon)
+        button_open = tkinter.Button(self, text="Otevřít", image=self.file_open_icon)
+        button_save = tkinter.Button(self, text="Uložito", image=self.file_save_icon)
+
+        button_zoom_in = tkinter.Button(self, text="Zvětšit", image=self.zoom_in_icon)
+        button_zoom_out = tkinter.Button(self, text="Zmenšit", image=self.zoom_out_icon)
+        button_zoom_11 = tkinter.Button(self, text="1:1", image=self.zoom_original_icon)
+
+        button_room_list = tkinter.Button(self, text="Room list", image=self.room_list_icon)
+
+        spacer1 = tkinter.Label(self, text="  ")
+        spacer2 = tkinter.Label(self, text="  ")
+
+        button_import.grid(column=1, row=1)
+        button_open.grid(column=2, row=1)
+        button_save.grid(column=3, row=1)
+
+        spacer1.grid(column=4, row=1)
+
+        button_zoom_in.grid(column=5, row=1)
+        button_zoom_out.grid(column=6, row=1)
+        button_zoom_11.grid(column=7, row=1)
+
+        spacer2.grid(column=8, row=1)
+
+        button_room_list.grid(column=9, row=1)
