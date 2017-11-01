726d78139144067143eff704506554621864b3e1
Pavel Tisnovsky <ptisnovs@redhat.com>
2017-11-02 00:22:14 +0100

    New icon - view boundary

---
+++ src/icons/view_boundary.py
@@ -0,0 +1,17 @@
+# This icon was converted from the Oxygen Icon Theme
+# Please see copyright for further informations
+
+icon = """
+iVBORw0KGgoAAAANSUhEUgAAABYAAAAWCAYAAADEtGw7AAABI1BMVEX/////////////////////
+//8AWQAAaAD/AAADXgEDgAIDhQMHZwQJfAUQdQkScxASfgoTdgoZew0aiw0jmRMkiRMnjRQvmhkx
+mRo0mRs5njk7ryE8qCBEqSRPtClVVVVYmlRarVp9xmiAs/+Ix4iKyACSxQKazwGbzAKczwKc2Qqd
+zgOh1gWi1QWk2gal2Z2n2gio3Qus2gOs3Jmtzv+00v+7u7u8vLy85K+9vb2+vr6/v7/AwMDBwcHC
+wsLDw8PExMTFxcXGxsbHx8fIyMjJycnKysrLy8vMzMzOzs7Pz8/Q0NDR0dHS0tLT09PY2NjZ2dna
+2tro6Ojv9+7y8vL09PT0+PT19fX2+vb39/f4+/f4/Pf4/Pj5/Pn6/fn7/vr///9gwTRrAAABI0lE
+QVQ4y9VVzWqDQBgcS0Evi1fx4nM0NCQ0Dy4oaLXBmnoyxYM3X0C8yE4Pkq0aWs2Ph3zwHcaBXWeY
+YTUCxALzDADfxyNIgiSapgFJSCnVt/EahvEv/7JadQeThGmaEEIgz3PYtq1uLssSjuMoXBTFJK/+
+mPx1Q0oJIYTCJC/GAKAR4NfhoGScyL7Utm0HUnVd/9MGknhdrwECTNOUVVWxrmsmScKqqtRGUcT+
+eJ43wEEQnPMAZ1nRn7lYI8DPJFHST2R/L7Vis912VmRZpqwIw3Agzff9AXZdd5oH+DSWc630q1Jx
+U0Esy3qggkylYryzC7Lf71Uq4jieLsCMgiybio84PkvFLVaogkRR9GAFeQ/Du1rxttt1By/x5mlj
+KfeaHyj1XMufcSnyAAAAAElFTkSuQmCC
+"""
