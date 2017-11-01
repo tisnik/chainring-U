b8f90b41f3efe4f1bdad4db3a413c973e3c719c8
Pavel Tisnovsky <ptisnovs@redhat.com>
2017-11-02 00:22:25 +0100

    New icon - view grid

---
+++ src/icons/view_grid.py
@@ -0,0 +1,24 @@
+# This icon was converted from the Oxygen Icon Theme
+# Please see copyright for further informations
+
+icon = """
+iVBORw0KGgoAAAANSUhEUgAAABYAAAAWCAMAAADzapwJAAACWFBMVEUAAAAKCgoICAgJCQkAAAAA
+AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAXf8AXv8AX/8AYP8AYf8AYv+lxOmn
+xumsyeqsyuqty+utzPCuy+yvzvGxz++y0PCz0PGz0fG00PG00fK00vG20/S30/S31PW41vW51vW5
+1va51ve61ve61/i62Pm92fq+2vq+2vu/2vu/2/u/2/zA3P3B3f3W4fDX4/Ha5/Tb6PTc6PXd3+Dd
+5vLe3+De6PTe6fTg6vXh6/Xh6/bh7fri4N3i7Pbj4+Tj7Pfj7ffk4d7k5Obk5eXk7vjk7/nl4d7l
+5uPl5ufl7vnl7/nl8f7m49/m5ufm5+bm8frn8Pro5eHo5eLo6ejo6eno8Pvo8fzo8v3p5uPp5+Xp
+6urp8v3q6+vq9f7r6OPr6urr7Ozr9P7r9f7s6eTs6eXs7ezt5+Dt6eXt7ezt7e3t7u3t7+/u6OLu
+7Oju7evu7+7u8O/v7Ojv7+/v8O/v8PDv8fHw7enw7+3w7+7w8O/w8PDx7unx7ury7+ry7+vy8e7y
+8fDy8vLz7eXz7+nz8Ovz8Oz09PP09PT19fT19vb28+/29fT29vX29vb39O/39vP39vX39/b39/f3
++Pj49O/49fH49/T49/f4+Pf59fH59vL5+fn69e769vH69vL69/P79/L79/P7+vj7+vn7+/v89u/8
++/r8/Pz9+PP9/Pv9/f3++PH++vX++vb+/fr/+/T//Pf//Pj//fb//fj//fn//vn//vr//v3///r/
+//v///z///3///7////jN64KAAAAEXRSTlMAEhkaNTZCQ0RFRkdOT1BRU/CrYSwAAAFMSURBVHgB
+Y8AJGFlZUAArI1iY8zga4AYLcxx3K7CIDvAusyzMyOu1qj7OBRUWtxOzk5R2EHWSV3AWsYEKsx+3
+z7VLDgxvdWksqpjh1QkXto2R8omIa1PMLy6dotaEEE6SDY9N6VOpqmuZr9EFFw7rcWxv61/vtXz+
+kp3+m+DCEg6iLsoqXiLBWjpRwq5wYY8Ol87K5lUhC6bN2hW/Bi7s26BUUtOyRHva9Dm7DddChdmO
+B3Wo1tZ3r9ZbtHDpfpMNcOHyZV4rFq/cE7l3/4HDiYfhwjLuIn6aupHCCUamqUKhcOHMeX4zp87e
+m7Bj8+Yj2UfhwmlT1CdMnLPbaPO6jUfMDsKFs2ZoTJo8d5/x5o1bjpofgwv3b/Xftn33ocQjR44e
+zUFYKecpFKJvkCCYamaeLmQN8w722GHm5eHh4QUCPiDg5+cXYGJgAACBuJstdfbCLgAAAABJRU5E
+rkJggg==
+"""
