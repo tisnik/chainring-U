15a1075670ac85fe60b5d99cf61f0e23e47ae3d0
Pavel Tisnovsky <ptisnovs@redhat.com>
2017-11-06 00:12:42 +0100

    Call menubar and toolbar constructors with proper parameters

--- src/gui/main_window.py
+++ src/gui/main_window.py
@@ -28,11 +28,11 @@ class MainWindow:
     def __init__(self):
         self.root = tkinter.Tk()
 
-        self.canvas = Canvas(self.root, 800, 600)
-        self.toolbar = Toolbar(self.root)
+        self.canvas = Canvas(self.root, 1000, 700)
+        self.toolbar = Toolbar(self.root, self, self.canvas)
         self.palette = Palette(self.root)
 
-        self.menubar = Menubar(self.root)
+        self.menubar = Menubar(self.root, self, self.canvas)
 
         self.root.config(menu=self.menubar)
 
@@ -40,8 +40,28 @@ class MainWindow:
         self.palette.grid(column=1, row=2, sticky="NWSE")
         self.canvas.grid(column=2, row=2, sticky="NWSE")
 
+        self.canvas.bind("<ButtonPress-1>", self.scroll_start)
+        self.canvas.bind("<B1-Motion>", self.scroll_move)
+        # scroll on Linux
+        self.canvas.bind("<Button-4>", self.zoom_plus)
+        self.canvas.bind("<Button-5>", self.zoom_minus)
+        # scroll on windows
+        self.canvas.bind("<MouseWheel>",self.zoom)
+
+    def quit(self):
+        answer = messagebox.askyesno("Skutečně ukončit program?",
+                                     "Skutečně ukončit program?")
+        if answer:
+            self.root.quit()
+
     def show(self):
         self.root.mainloop()
 
-    def draw_entities(self, entities, xoffset, yoffset, scale):
-        self.canvas.draw_entities(entities, xoffset, yoffset, scale)
+    def set_entities(self, entities):
+        self.entities = entities
+
+    def redraw(self):
+        self.canvas.delete("all")
+        self.canvas.draw_grid()
+        self.canvas.draw_boundary()
+        self.canvas.draw_entities(self.entities, 0, 0, 1)
