76faf208539a7788b1c798899728f280854b416c
Pavel Tisnovsky <ptisnovs@redhat.com>
2017-11-09 18:01:47 +0100

    Updated docstrings

--- src/geometry/bounds.py
+++ src/geometry/bounds.py
@@ -10,25 +10,32 @@
 #      Pavel Tisnovsky
 #
 
+"""Module with class that represents bounds of one entity or entity group."""
+
 import sys
 
 
 class Bounds():
+    """Class representing bounds of given entity or group of entitites."""
+
     def __init__(self,
                  xmin=sys.float_info.max, ymin=sys.float_info.max,
                  xmax=-sys.float_info.max, ymax=-sys.float_info.max):
+        """Construct new bounds using given coordinates or default values."""
         self.xmin = xmin
         self.ymin = ymin
         self.xmax = xmax
         self.ymax = ymax
 
     def enlarge(self, other):
+        """Enlarge the area represented by bound."""
         self.xmin = min(self.xmin, other.xmin)
         self.ymin = min(self.ymin, other.ymin)
         self.xmax = max(self.xmax, other.xmax)
         self.ymax = max(self.ymax, other.ymax)
 
     def __repr__(self):
+        """Return textual representation of the bound."""
         return "[{xmin}, {ymin}] - [{xmax}, {ymax}]".format(
             xmin=self.xmin,
             ymin=self.ymin,
@@ -37,6 +44,7 @@ class Bounds():
 
     @staticmethod
     def computeBounds(entities):
+        """Compute bounds for all given entities."""
         bounds = Bounds()
         for entity in entities:
             bounds.enlarge(entity.getBounds())
