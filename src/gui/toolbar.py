db4b28e98542e9f2ddee0367d319b62451e2b939
Pavel Tisnovsky <ptisnovs@redhat.com>
2017-11-15 00:22:12 +0100

    New room button + fixed Pylinter warnings

--- src/gui/toolbar.py
+++ src/gui/toolbar.py
@@ -12,25 +12,57 @@
 
 import tkinter
 
+from gui.settings_dialog import SettingsDialog
+
 
 class Toolbar(tkinter.LabelFrame):
     def __init__(self, parent, main_window, canvas):
         super().__init__(parent, text="Nástroje", padx=5, pady=5)
 
-        button_import = tkinter.Button(self, text="Import", image=main_window.icons.file_import_icon)
-        button_open = tkinter.Button(self, text="Otevřít", image=main_window.icons.file_open_icon)
-        button_save = tkinter.Button(self, text="Uložit", image=main_window.icons.file_save_icon)
-        button_quit = tkinter.Button(self, text="Ukončit", image=main_window.icons.exit_icon, command=main_window.quit)
+        self.parent = parent
 
-        button_zoom_in = tkinter.Button(self, text="Zvětšit", image=main_window.icons.zoom_in_icon, command=main_window.zoom_plus)
-        button_zoom_out = tkinter.Button(self, text="Zmenšit", image=main_window.icons.zoom_out_icon, command=main_window.zoom_minus)
-        button_zoom_11 = tkinter.Button(self, text="1:1", image=main_window.icons.zoom_original_icon, command=main_window.redraw)
+        button_import = tkinter.Button(self, text="Import",
+                                       image=main_window.icons.file_import_icon)
 
-        button_view_grid = tkinter.Button(self, text="Mřížka", image=main_window.icons.view_grid_icon, command=canvas.toggle_grid)
-        button_view_boundary = tkinter.Button(self, text="Okraj", image=main_window.icons.view_boundary_icon, command=canvas.toggle_boundary)
+        button_open = tkinter.Button(self, text="Otevřít",
+                                     image=main_window.icons.file_open_icon)
 
-        button_room_list = tkinter.Button(self, text="Seznam místností", image=main_window.icons.room_list_icon)
-        button_new_room = tkinter.Button(self, text="Nakreslit místnost", image=main_window.icons.edit_icon)
+        button_save = tkinter.Button(self, text="Uložit",
+                                     image=main_window.icons.file_save_icon)
+
+        button_quit = tkinter.Button(self, text="Ukončit",
+                                     image=main_window.icons.exit_icon,
+                                     command=main_window.quit)
+
+        button_zoom_in = tkinter.Button(self, text="Zvětšit",
+                                        image=main_window.icons.zoom_in_icon,
+                                        command=main_window.zoom_plus)
+
+        button_zoom_out = tkinter.Button(self, text="Zmenšit",
+                                         image=main_window.icons.zoom_out_icon,
+                                         command=main_window.zoom_minus)
+
+        button_zoom_11 = tkinter.Button(self, text="1:1",
+                                        image=main_window.icons.zoom_original_icon,
+                                        command=main_window.redraw)
+
+        button_view_grid = tkinter.Button(self, text="Mřížka",
+                                          image=main_window.icons.view_grid_icon,
+                                          command=canvas.toggle_grid)
+
+        button_view_boundary = tkinter.Button(self, text="Okraj",
+                                              image=main_window.icons.view_boundary_icon,
+                                              command=canvas.toggle_boundary)
+
+        button_room_list = tkinter.Button(self, text="Seznam místností",
+                                          image=main_window.icons.room_list_icon)
+
+        button_new_room = tkinter.Button(self, text="Nakreslit místnost",
+                                         image=main_window.icons.edit_icon)
+
+        button_settings = tkinter.Button(self, text="Nastavení",
+                                         image=main_window.icons.properties_icon,
+                                         command=self.show_settings_dialog)
 
         spacer1 = tkinter.Label(self, text="  ")
         spacer2 = tkinter.Label(self, text="  ")
@@ -52,7 +84,11 @@ class Toolbar(tkinter.LabelFrame):
         spacer2.grid(column=11, row=1)
 
         button_room_list.grid(column=12, row=1)
+        button_settings.grid(column=13, row=1)
 
-        spacer3.grid(column=13, row=1)
+        spacer3.grid(column=14, row=1)
 
-        button_new_room.grid(column=14, row=1)
+        button_new_room.grid(column=15, row=1)
+
+    def show_settings_dialog(self):
+        SettingsDialog(self.parent)
