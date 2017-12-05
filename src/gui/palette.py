d9f89f57d0280add85f145903d0a1d10dade95e9
Pavel Tisnovsky <ptisnovs@redhat.com>
2017-12-05 09:18:58 +0100

    Palette with room list + bindings for click

--- src/gui/palette.py
+++ src/gui/palette.py
@@ -14,9 +14,31 @@ import tkinter
 
 
 class Palette(tkinter.LabelFrame):
-    def __init__(self, parent):
+
+    def __init__(self, parent, main_window):
         super().__init__(parent, text="Výkres", padx=5, pady=5)
-        button = tkinter.Button(self, text="Test")
-        button.grid(column=1, row=1)
-        listbox = tkinter.Listbox(self)
-        listbox.grid(column=1, row=2, sticky="NWSE")
+        self.main_window = main_window
+
+        self.listbox = tkinter.Listbox(self)
+        self.listbox.pack(fill=tkinter.BOTH, expand=True)
+        self.group = tkinter.LabelFrame(self, text="Vybraná místnost",
+                                        padx=5, pady=5)
+
+        label1 = tkinter.Label(self.group, text="ID")
+        label1.grid(column=1, row=1, sticky="W")
+        label2 = tkinter.Label(self.group, text="Rohů")
+        label2.grid(column=1, row=2, sticky="W")
+        # button = tkinter.Button(self.group, text="Test")
+        # button.pack()
+        self.group.pack(fill=tkinter.X, expand=False)
+        self.listbox.bind("<<ListboxSelect>>", self.on_room_click)
+
+    def add_new_room(self, canvas_id):
+        self.listbox.insert(tkinter.END, canvas_id)
+
+    def on_room_click(self, event):
+        selection = self.listbox.curselection()
+        if selection:
+            index = selection[0]
+            value = self.listbox.get(index)
+            self.main_window.on_room_click_listbox(value)
