aa5438426ef210200a4129b88e42f7056ff80e45
Pavel Tisnovsky <ptisnovs@redhat.com>
2017-12-05 09:15:31 +0100

    Variations of dialogs displayed during room creation

---
+++ src/gui/room_error_dialog.py
@@ -0,0 +1,24 @@
+#
+#  (C) Copyright 2017  Pavel Tisnovsky
+#
+#  All rights reserved. This program and the accompanying materials
+#  are made available under the terms of the Eclipse Public License v1.0
+#  which accompanies this distribution, and is available at
+#  http://www.eclipse.org/legal/epl-v10.html
+#
+#  Contributors:
+#      Pavel Tisnovsky
+#
+
+import tkinter
+
+from tkinter import messagebox
+
+
+def error_dialog_no_points():
+    messagebox.showerror("Chyba při kreslení místnosti",
+                         "Nebyl zadán ani jeden bod")
+
+def error_dialog_not_enough_points():
+    messagebox.showerror("Chyba při kreslení místnosti",
+                         "Místnost musí být definována alespoň třemi body")
