0788ff7487f32c2e18e05aca7bfa2a4db51d2873
Pavel Tisnovsky <ptisnovs@redhat.com>
2017-12-05 09:14:56 +0100

    Class 'Room'

---
+++ src/gui/room.py
@@ -0,0 +1,29 @@
+#
+#  (C) Copyright 2017  Pavel Tisnovsky
+#
+#  All rights reserved. This program and the accompanying materials
+#  are made available under the terms of the Eclipse Public License v1.0
+#  which accompanies this distribution, and is available at
+#  http://www.eclipse.org/legal/epl-v10.html
+#
+#  Contributors:
+#      Pavel Tisnovsky
+#
+
+
+class Room:
+
+    def __init__(self):
+        self.cleanup()
+
+    def cleanup(self):
+        self.last_x = None
+        self.last_y = None
+        self.polygon_world = []
+        self.polygon_canvas = []
+
+    def last_point_exist(self):
+        return self.last_x and self.last_y
+
+    def polygons(self):
+        return len(self.polygon_world)
