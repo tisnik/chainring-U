23e1f2b43e1e4fc05bcbab7272fd443a20705e9e
Pavel Tisnovsky <ptisnovs@redhat.com>
2017-12-05 09:14:25 +0100

    Geometry utils module

---
+++ src/geometry/utils.py
@@ -0,0 +1,18 @@
+#
+#  (C) Copyright 2017  Pavel Tisnovsky
+#
+#  All rights reserved. This program and the accompanying materials
+#  are made available under the terms of the Eclipse Public License v1.0
+#  which accompanies this distribution, and is available at
+#  http://www.eclipse.org/legal/epl-v10.html
+#
+#  Contributors:
+#      Pavel Tisnovsky
+#
+
+
+class GeometryUtils:
+
+    @staticmethod
+    def square_length(x1, y1, x2, y2):
+        return (x1-x2)**2 + (y1-y2)**2
