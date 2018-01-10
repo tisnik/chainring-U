bd333ddd26d56c13d2f073b6a9f7c618bd4d8032
Pavel Tisnovsky <ptisnovs@redhat.com>
2018-01-10 13:55:11 +0100

    Fixed docstyle issues

--- src/importers/binary_importer.py
+++ src/importers/binary_importer.py
@@ -1,5 +1,5 @@
 #
-#  (C) Copyright 2017  Pavel Tisnovsky
+#  (C) Copyright 2017, 2018  Pavel Tisnovsky
 #
 #  All rights reserved. This program and the accompanying materials
 #  are made available under the terms of the Eclipse Public License v1.0
@@ -10,6 +10,8 @@
 #      Pavel Tisnovsky
 #
 
+"""Importer for drawings stored in a binary format."""
+
 import pickle
 
 
@@ -17,9 +19,10 @@ class BinaryImporter:
     """Importer for drawings stored in a binary format."""
 
     def __init__(self, filename):
+        """Initialize the importer for drawings stored in binary format."""
         self.filename = filename
 
     def import_binary_drawing(self):
-        '''Import the binary file and return structure with all entities.'''
+        """Import the binary file and return structure with all entities."""
         with open(self.filename, "rb") as fin:
             return pickle.load(fin)
