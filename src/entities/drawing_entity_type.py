e510599d20bc3b3e68880cbbc3a58f2f000e8638
Pavel Tisnovsky <ptisnovs@redhat.com>
2018-01-17 11:14:10 +0100

    Fixed pydocstyle warnings

--- src/entities/drawing_entity_type.py
+++ src/entities/drawing_entity_type.py
@@ -1,5 +1,5 @@
 #
-#  (C) Copyright 2017  Pavel Tisnovsky
+#  (C) Copyright 2017, 2018  Pavel Tisnovsky
 #
 #  All rights reserved. This program and the accompanying materials
 #  are made available under the terms of the Eclipse Public License v1.0
@@ -10,10 +10,15 @@
 #      Pavel Tisnovsky
 #
 
+"""All entity types that can be used in drawings."""
+
+
 from enum import Enum
 
 
 class DrawingEntityType(Enum):
+    """All entity types that can be used in drawings."""
+
     UNKNOWN = 1,
     LINE = 2,
     CIRCLE = 3,
