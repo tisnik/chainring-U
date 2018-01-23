e0ef2bc0fc83ed2dd1e8598b6ae82afdf99dee49
Pavel Tisnovsky <ptisnovs@redhat.com>
2018-01-23 11:28:30 +0100

    Rescaler to compute scale for given width and height

--- src/geometry/rescaler.py
+++ src/geometry/rescaler.py
@@ -1,13 +1,17 @@
 class Rescaler:
 
     @staticmethod
-    def computeScale(bounds, canvas):
-        canvas_width = canvas.winfo_reqwidth()
-        canvas_height = canvas.winfo_reqheight()
+    def computeScale(bounds, width, height):
         xdist = bounds.xmax - bounds.xmin
         ydist = bounds.ymax - bounds.ymin
-        xscale = 0.99 * canvas_width / xdist
-        yscale = 0.99 * canvas_height / ydist
+        xscale = 0.99 * width / xdist
+        yscale = 0.99 * height / ydist
         scale = min(xscale, yscale)
         return -bounds.xmin, -bounds.ymin, scale
+
+    @staticmethod
+    def computeScaleForCanvas(bounds, canvas):
+        canvas_width = canvas.winfo_reqwidth()
+        canvas_height = canvas.winfo_reqheight()
+        return Rescaler.computeScale(bounds, canvas_width, canvas_height)
 
