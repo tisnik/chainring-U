202e95e7f337a58ea17df8f8b9a1cd512f74f3a1
Pavel Tisnovsky <ptisnovs@redhat.com>
2018-02-07 21:35:34 +0100

    Updated settings dialog

--- src/gui/settings_dialog.py
+++ src/gui/settings_dialog.py
@@ -1,5 +1,5 @@
 #
-#  (C) Copyright 2017  Pavel Tisnovsky
+#  (C) Copyright 2017, 2018  Pavel Tisnovsky
 #
 #  All rights reserved. This program and the accompanying materials
 #  are made available under the terms of the Eclipse Public License v1.0
@@ -22,19 +22,46 @@ class SettingsDialog(tkinter.Toplevel):
         # don't display the dialog in list of opened windows
         self.transient(parent)
 
-        label1 = tkinter.Label(self, text="First name")
-        label2 = tkinter.Label(self, text="Surname")
+        self.group1 = tkinter.LabelFrame(self, text="Nastavení serveru", padx=5, pady=8)
+        self.group2 = tkinter.LabelFrame(self, text="Informace o uživateli", padx=5, pady=8)
+        self.group3 = tkinter.LabelFrame(self, text="Zobrazení výkresu", padx=5, pady=8)
 
-        self.entryFirstName = tkinter.Entry(self)
-        self.entrySurname = tkinter.Entry(self)
-
-        okButton = tkinter.Button(self, text="OK", command=self.ok)
+        # frame group #1
+        label1 = tkinter.Label(self.group1, text="Adresa")
+        label2 = tkinter.Label(self.group1, text="Port")
+        self.entryServerAddress = tkinter.Entry(self.group1)
+        self.entryServerPort = tkinter.Entry(self.group1)
 
         label1.grid(row=1, column=1, sticky="W", padx=5, pady=5)
         label2.grid(row=2, column=1, sticky="W", padx=5, pady=5)
-        self.entryFirstName.grid(row=1, column=2, sticky="WE")
-        self.entrySurname.grid(row=2, column=2, sticky="WE")
-        okButton.grid(row=4, column=2, sticky="W")
+        self.entryServerAddress.grid(row=1, column=2, sticky="WE")
+        self.entryServerPort.grid(row=2, column=2, sticky="WE")
+
+        self.group1.grid(row=1, column=1, sticky="WE")
+
+        # frame group #2
+        label3 = tkinter.Label(self.group2, text="Uživatelské jméno")
+        label3.grid(row=1, column=1, sticky="W", padx=5, pady=5)
+        self.entryLogin = tkinter.Entry(self.group2)
+        self.entryLogin.grid(row=1, column=2, sticky="WE")
+        self.group2.grid(row=2, column=1, sticky="WE")
+
+        # frame group #3
+        label4 = tkinter.Label(self.group3, text="Barva pozadí")
+        label5 = tkinter.Label(self.group3, text="Barva půdorysu")
+        label6 = tkinter.Label(self.group3, text="Barva kreslení")
+        label7 = tkinter.Label(self.group3, text="Barva místností")
+        label8 = tkinter.Label(self.group3, text="Barva vybrané místnosti")
+        label4.grid(row=1, column=1, sticky="W", padx=5, pady=5)
+        label5.grid(row=2, column=1, sticky="W", padx=5, pady=5)
+        label6.grid(row=3, column=1, sticky="W", padx=5, pady=5)
+        label7.grid(row=4, column=1, sticky="W", padx=5, pady=5)
+        label8.grid(row=5, column=1, sticky="W", padx=5, pady=5)
+        self.group3.grid(row=3, column=1, sticky="WE")
+
+        # rest
+        okButton = tkinter.Button(self, text="OK", command=self.ok)
+        okButton.grid(row=4, column=1, sticky="W")
 
         # close the dialog on 'x' click
         self.protocol("WM_DELETE_WINDOW", self.destroy)
@@ -47,9 +74,10 @@ class SettingsDialog(tkinter.Toplevel):
         self.bind("<Escape>", lambda event: self.destroy())
 
         # get the focus
-        self.entryFirstName.focus_set()
+        okButton.focus_set()
 
     def ok(self):
-        print("first name:", self.entryFirstName.get())
-        print("surname:", self.entrySurname.get())
+        print("server address:", self.entryServerAddress.get())
+        print("server port:", self.entryServerPort.get())
+        print("username:", self.entryLogin.get())
         self.destroy()
