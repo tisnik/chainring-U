f6012ee31de9e208c9bf612efa917336f7d98c23
Pavel Tisnovsky <ptisnovs@redhat.com>
2018-08-13 16:09:25 +0200

    Cleanup in the main module

--- src/chainring.py
+++ src/chainring.py
@@ -20,6 +20,7 @@ from exporters.json_exporter import *
 from geometry.bounds import Bounds
 from geometry.rescaler import Rescaler
 from configuration import *
+from gui.load_dialogs import LoadDialogs
 
 import sys
 
@@ -27,37 +28,48 @@ configuration = Configuration()
 #configuration.write()
 
 mainWindow = MainWindow(configuration)
+drawing = None
 
-importer = DrawingImporter("input.drw")
-drawing = importer.import_drawing()
+#importer = DrawingImporter("input.drw")
+#drawing = importer.import_drawing()
 #importer = DxfImporter("test-data/Building_1np.dxf")
 #importer = DxfImporter("test2.dxf")
 #importer = DxfImporter("7701_cad.dxf")
 #drawing = importer.import_dxf()
 
-exporter = DrawingExporter("output.drw", drawing)
-exporter.export()
+# exporter = DrawingExporter("output.drw", drawing)
+# exporter.export()
 
-json_exporter = JSONExporter("output.json", drawing)
-json_exporter.export()
+# json_exporter = JSONExporter("output.json", drawing)
+# json_exporter.export()
 
-bounds = Bounds.computeBounds(drawing.entities)
-print(bounds)
-xoffset, yoffset, scale = Rescaler.computeScaleForCanvas(bounds, mainWindow.canvas)
-print(xoffset, yoffset, scale)
+if drawing is None:
+    drawing_file_name = LoadDialogs.load_drawing(None)
+    if drawing_file_name is not None and drawing_file_name != "" and drawing_file_name != ():
+        print(drawing_file_name)
+        importer = DrawingImporter(drawing_file_name)
+        drawing = importer.import_drawing()
+    pass
 
-#ex = BinaryExporter("output3.bin", entities)
-#ex.export_binary_drawing()
+if drawing is not None:
+    bounds = Bounds.computeBounds(drawing.entities)
+    print(bounds)
+    xoffset, yoffset, scale = Rescaler.computeScaleForCanvas(bounds, mainWindow.canvas)
+    print(xoffset, yoffset, scale)
 
-#with open("output.bin", "wb") as fout:
-#    pickle.dump(entities, fout)
+    #ex = BinaryExporter("output3.bin", entities)
+    #ex.export_binary_drawing()
 
-drawing.rescale(xoffset, yoffset, scale)
+    #with open("output.bin", "wb") as fout:
+    #    pickle.dump(entities, fout)
 
-exporter = DrawingExporter("output2.drw", drawing)
-exporter.export()
+    drawing.rescale(xoffset, yoffset, scale)
+
+    # exporter = DrawingExporter("output2.drw", drawing)
+    # exporter.export()
 
 mainWindow.drawing = drawing
 mainWindow.redraw()
 mainWindow.add_all_rooms_from_drawing()
+mainWindow.set_ui_items_for_actual_mode()
 mainWindow.show()
