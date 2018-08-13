93a4eee43dbd4086c267ba4cdebb5db91e428ad2
Pavel Tisnovsky <ptisnovs@redhat.com>
2018-08-13 08:22:34 +0200

    Implementation of room list dialog

--- src/gui/dialogs/room_list_dialog.py
+++ src/gui/dialogs/room_list_dialog.py
@@ -11,15 +11,56 @@
 #
 
 import tkinter
+from tkinter.scrolledtext import ScrolledText
 
 
 class RoomListDialog(tkinter.Toplevel):
+
     def __init__(self, parent, drawing):
         tkinter.Toplevel.__init__(self, parent)
         self.title("Seznam místností")
 
+        rooms = drawing.rooms
+        rooms_count = len(rooms)
+
+        self.add_label(0, 0, "Počet místností")
+        self.add_value_widget(0, 1, rooms_count)
+
+        self.txt = ScrolledText(self, undo=True, tabs="4c 7c 10c 24c")
+        self.txt.grid(row=1, column=0, columnspan=2, sticky="nwse", padx=2, pady=2)
+
+        self.txt.tag_configure("underlined", underline=True)
+        self.txt.tag_configure("blue", foreground="blue")
+        self.txt.tag_configure("green", foreground="green")
+        self.txt.tag_configure("red", foreground="red")
+
+        self.txt.insert(tkinter.INSERT, "SAP ID\tgr.ID\tzakreslena\tsouřadnic\n", "underlined")
+
+        for room in rooms:
+            if "room_id" in room:
+                self.txt.insert(tkinter.END, room["room_id"] + "\t", "blue")
+            else:
+                self.txt.insert(tkinter.END, "x\t", "red")
+
+            if "canvas_id" in room:
+                self.txt.insert(tkinter.END, str(room["canvas_id"]) + "\t")
+            else:
+                self.txt.insert(tkinter.END, "x\t", "red")
+
+            if room["polygon"] is not None:
+                self.txt.insert(tkinter.END, "ano\t", "green")
+            else:
+                self.txt.insert(tkinter.END, "ne\t", "red")
+
+            if room["polygon"] is not None:
+                self.txt.insert(tkinter.END, str(len(room["polygon"])) + "\n")
+            else:
+                self.txt.insert(tkinter.END, "?\n")
+
+        self.txt.configure(state="disabled")
+
         okButton = tkinter.Button(self, text="OK", command=self.ok)
-        okButton.grid(row=3, column=1, sticky="WE")
+        okButton.grid(row=2, column=0, sticky="WE")
 
         # close the dialog on 'x' click
         self.protocol("WM_DELETE_WINDOW", self.destroy)
@@ -37,6 +78,16 @@ class RoomListDialog(tkinter.Toplevel):
     def ok(self):
         self.destroy()
 
-    @staticmethod
-    def compute_sum(drawing_statistic):
-        return sum(drawing_statistic.values())
+    def add_label(self, row, column, text):
+        label = tkinter.Label(self, text=text)
+        label.grid(row=row, column=column, sticky="W", padx=5, pady=5)
+
+    def add_value_widget(self, row, column, value):
+        widget = self.value_widget(value)
+        widget.grid(row=row, column=column, sticky="W", padx=5, pady=5)
+
+    def value_widget(self, value):
+        widget = tkinter.Entry(self)
+        widget.insert(tkinter.END, value)
+        widget.configure(state='readonly')
+        return widget
