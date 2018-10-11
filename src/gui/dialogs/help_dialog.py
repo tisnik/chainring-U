6f345cd350f1b8420ed584cf3f317c79d06c40e8
Pavel Tisnovsky <ptisnovs@redhat.com>
2018-10-11 16:14:56 +0200

    1st version of help window + fixed room list

--- src/gui/dialogs/help_dialog.py
+++ src/gui/dialogs/help_dialog.py
@@ -15,6 +15,49 @@ import tkinter
 from tkinter import messagebox
 
 
+class HelpDialog(tkinter.Toplevel):
+    def __init__(self, parent):
+        tkinter.Toplevel.__init__(self, parent)
+        self.title("Nápověda")
+        self.transient(parent)
+
+        f = tkinter.LabelFrame(self, text="x")
+
+        scrollbar = tkinter.Scrollbar(f)
+        text = tkinter.Text(f, height=5, width=60)
+
+        scrollbar.pack(side=tkinter.RIGHT, fill=tkinter.Y)
+        text.pack(side=tkinter.LEFT, fill=tkinter.BOTH, expand=1)
+        scrollbar.config(command=text.yview)
+        text.config(yscrollcommand=scrollbar.set)
+
+        text.tag_configure("h1", font=("Arial", 20, "bold"))
+        text.tag_configure("h2", font=("Arial", 16, "bold"))
+
+        text.insert(tkinter.END, "Nápověda\n", "h1")
+        text.insert(tkinter.END, "Import výkresu\n", "h2")
+        text.insert(tkinter.END, "Získání místností ze SAPu\n", "h2")
+        text.insert(tkinter.END, "Nakreslení místností\n", "h2")
+        text.insert(tkinter.END, "Export výkresu do systému\n", "h2")
+
+        help_message = """"""
+        text.insert(tkinter.END, help_message)
+
+        text.config(state=tkinter.DISABLED)
+        f.grid(row=0, column=0, sticky="NWSE")
+
+        self.grid_rowconfigure(0, weight=1)
+        self.grid_columnconfigure(0, weight=1)
+
+        # rest
+        okButton = tkinter.Button(self, text="OK", command=self.ok)
+        okButton.grid(row=1, column=0, sticky="NWSE")
+
+        # close the dialog on 'x' click
+        self.protocol("WM_DELETE_WINDOW", self.destroy)
+
+    def ok(self):
+        self.destroy()
+
 def help():
-    messagebox.showinfo("Nápověda",
-                        "")
+    HelpDialog(None)
