30ba2920bc52df1c99ca6a229d8d96d0606a66ec
Pavel Tisnovsky <ptisnovs@redhat.com>
2018-10-15 09:25:17 +0200

    Support for load areals, buildins, floors, and rooms from SAP

--- src/draw_service.py
+++ src/draw_service.py
@@ -12,6 +12,8 @@
 
 import requests
 
+from exporters.json_exporter import *
+
 
 class DrawServiceInterface:
     API_PREFIX = "api/v1"
@@ -27,7 +29,7 @@ class DrawServiceInterface:
         url = "{url}/{api}/{endpoint}".format(url=self._service_url,
                                               api=DrawServiceInterface.API_PREFIX,
                                               endpoint=endpoint)
-        response = requests.get(url)
+        response = requests.get(url, timeout=10)
         return response.status_code, response.json()
 
     def check_liveness(self):
@@ -52,41 +54,60 @@ class DrawServiceInterface:
         except Exception as e:
             return False, None, repr(e)
 
-    def read_areals(self, valid_from):
+    def read_aoid(self, url, selector, error_message):
         try:
-            code, data = self.get("areals?valid-from=" + valid_from)
+            code, data = self.get(url)
             if code != 200:
                 return None, "Návratový kód {code}".format(code=code)
-            print(data)
             if "status" in data and data["status"] == "ok":
-                if "areals" in data:
-                    return data["areals"], "Ok"
+                if selector in data:
+                    return data[selector], "Ok"
                 else:
-                    return None, "Seznam areálů je prázdný"
+                    return None, error_message
             return None, "Neplatná data vrácená serverem"
         except Exception as e:
             return None, repr(e)
 
+    def read_areals(self, valid_from):
+        url = "areals?valid-from={date}".format(date=valid_from)
+        return self.read_aoid(url, "areals", "Seznam areálů je prázdný")
+
     def read_buildings(self, valid_from, aoid):
-        try:
-            code, data = self.get("buildings?valid-from=" + valid_from + "&areal-id=" + aoid)
-            if code != 200:
-                return None, "Návratový kód {code}".format(code=code)
-            print(data)
-            if "status" in data and data["status"] == "ok":
-                if "buildings" in data:
-                    return data["buildings"], "Ok"
-                else:
-                    return None, "Seznam budov je prázdný"
-            return None, "Neplatná data vrácená serverem"
-        except Exception as e:
-            return None, repr(e)
+        url = "buildings?valid-from={date}&areal-id={aoid}".format(date=valid_from, aoid=aoid)
+        return self.read_aoid(url, "buildings", "Seznam budov je prázdný")
+
+    def read_floors(self, valid_from, aoid):
+        url = "floors?valid-from={date}&building-id={aoid}".format(date=valid_from, aoid=aoid)
+        return self.read_aoid(url, "floors", "Seznam podlaží je prázdný")
+
+    def read_rooms(self, valid_from, aoid):
+        url = "rooms?valid-from={date}&floor-id={aoid}".format(date=valid_from, aoid=aoid)
+        return self.read_aoid(url, "rooms", "Seznam místností je prázdný")
 
     def check_input_data(self, data):
         return "projects" in data and \
                "buildings" in data and \
                "floors" in data and \
                "drawings" in data
+
+    def send_drawing(self, drawing):
+        endpoint = "drawing-data?drawing={id}&format=json".format(id=drawing.drawing_id)
+        url = "{url}/{api}/{endpoint}".format(url=self._service_url,
+                                              api=DrawServiceInterface.API_PREFIX,
+                                              endpoint=endpoint)
+        json_exporter = JSONExporter("output.json", drawing)
+        payload = json_exporter.as_string()
+        try:
+            response = requests.post(url, data=payload, timeout=30)
+            code, data = self.get("info")
+            print(code, data)
+            if code != 200:
+                return False, "Návratový kód {code}".format(code=code)
+            else:
+                message = "Výkres byl uložen pod ID {id} ({b} bajtů)".format(id=drawing.drawing_id, b=len(payload))
+                return True, message
+        except Exception as e:
+            return False, e
 
     def read_all_drawings(self):
         try:
