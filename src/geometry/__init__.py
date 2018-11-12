b454a698cb7a2e2178c4b29be7107661b50e6502
Pavel Tisnovsky <ptisnovs@redhat.com>
2018-11-12 21:50:20 +0100

    Module description

--- src/geometry/__init__.py
+++ src/geometry/__init__.py
@@ -1,5 +1,5 @@
 #
-#  (C) Copyright 2017  Pavel Tisnovsky
+#  (C) Copyright 2017, 2018  Pavel Tisnovsky
 #
 #  All rights reserved. This program and the accompanying materials
 #  are made available under the terms of the Eclipse Public License v1.0
@@ -9,3 +9,5 @@
 #  Contributors:
 #      Pavel Tisnovsky
 #
+
+"""Geometry related classes and utility functions."""
