63d12b5384c2c6dc3e2b44a37b34deedd6915c4b
Pavel Tisnovsky <ptisnovs@redhat.com>
2018-11-12 21:47:39 +0100

    Docstrings for rescaler module

--- src/geometry/rescaler.py
+++ src/geometry/rescaler.py
@@ -1,5 +1,5 @@
 #
-#  (C) Copyright 2017  Pavel Tisnovsky
+#  (C) Copyright 2017, 2018  Pavel Tisnovsky
 #
 #  All rights reserved. This program and the accompanying materials
 #  are made available under the terms of the Eclipse Public License v1.0
@@ -10,11 +10,14 @@
 #      Pavel Tisnovsky
 #
 
+"""Class to recompute scale from canvas size and computed bounds."""
 
 class Rescaler:
+    """Class to recompute scale from canvas size and computed bounds."""
 
     @staticmethod
     def computeScale(bounds, width, height):
+        """Compute scale from given bounds, width, and height."""
         xdist = bounds.xmax - bounds.xmin
         ydist = bounds.ymax - bounds.ymin
         xscale = 0.99 * width / xdist
@@ -24,6 +27,7 @@ class Rescaler:
 
     @staticmethod
     def computeScaleForCanvas(bounds, canvas):
+        """Compute scale from given bounds and canvas (with width+height)."""
         canvas_width = canvas.winfo_reqwidth()
         canvas_height = canvas.winfo_reqheight()
         return Rescaler.computeScale(bounds, canvas_width, canvas_height)
