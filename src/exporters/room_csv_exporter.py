f6fa01848fcf26379b1528f8a72ccc028224f49a
Pavel Tisnovsky <ptisnovs@redhat.com>
2019-08-14 11:37:49 +0200

    Implementation of CSV exporter

--- src/exporters/room_csv_exporter.py
+++ src/exporters/room_csv_exporter.py
@@ -1,7 +1,7 @@
 """Room exporter (serializer) to CSV."""
 
 #
-#  (C) Copyright 2017, 2018  Pavel Tisnovsky
+#  (C) Copyright 2017, 2018, 2019  Pavel Tisnovsky
 #
 #  All rights reserved. This program and the accompanying materials
 #  are made available under the terms of the Eclipse Public License v1.0
@@ -12,9 +12,31 @@
 #      Pavel Tisnovsky
 #
 
+import csv
 
 class RoomCSVExporter:
     """Room exporter (serializer) to CVS."""
 
-    # TODO: implementation
-    pass
+    def __init__(self, filename, drawing):
+        """Initialize the exporter, set the filename to be created and a sequence of entities."""
+        self.filename = filename
+        self.rooms = drawing.rooms
+
+    def export(self):
+        rooms = self.rooms
+        with open(self.filename, mode='w') as fout:
+            writer = csv.writer(fout, delimiter=',', quotechar='"', quoting=csv.QUOTE_ALL)
+            writer.writerow(("SAP ID", "gr.ID", "Zakreslena", "Souřadnic"))
+            for room in rooms:
+                room_id = room.get("room_id", "")
+                canvas_id = room.get("canvas_id", "")
+                drawn = "ano" if room["polygon"] is not None and len(room["polygon"]) > 0 else "ne"
+                poly = None
+                if room["polygon"] is not None:
+                    typ = "?"
+                    if "type" in room:
+                        typ = room["type"]
+                        poly = str(len(room["polygon"])) + " (" + typ + ")"
+                else:
+                    poly = "?"
+                writer.writerow((room_id, canvas_id, drawn, poly))
