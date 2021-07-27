4dcc1a8f8188675e42da1ab7af9c5319f993d1d6
Pavel Tisnovsky <ptisnovs@redhat.com>
2021-07-27 11:36:12 +0200

    Fixed issue #50

--- src/icons/edit_delete_shred.py
+++ src/icons/edit_delete_shred.py
@@ -1,4 +1,4 @@
-"""Icon for the following action: delete something"""
+"""Icon for the following action: delete something."""
 
 # This icon was converted from the Oxygen Icon Theme
 # Please see copyright for further informations
