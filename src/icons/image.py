ec4e8055acee7b5f45e3dc7fb815c08750490db5
Pavel Tisnovsky <ptisnovs@redhat.com>
2021-07-27 11:53:11 +0200

    Fixed issue #37

--- src/icons/image.py
+++ src/icons/image.py
@@ -1,4 +1,4 @@
-"""Icon for the following action: display image"""
+"""Icon for the following action: display image."""
 
 # This icon was converted from the Oxygen Icon Theme
 # Please see copyright for further informations
