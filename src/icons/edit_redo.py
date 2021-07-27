159995e9298600b587d698960d16d46d122da5df
Pavel Tisnovsky <ptisnovs@redhat.com>
2021-07-27 11:36:38 +0200

    Fixed issue #52

--- src/icons/edit_redo.py
+++ src/icons/edit_redo.py
@@ -1,4 +1,4 @@
-"""Icon for the following action: redo the last operation"""
+"""Icon for the following action: redo the last operation."""
 
 # This icon was converted from the Oxygen Icon Theme
 # Please see copyright for further informations
