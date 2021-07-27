ad3c4f4cb0d27c9d6fbcee7e46396a6c3ea371d1
Pavel Tisnovsky <ptisnovs@redhat.com>
2021-07-27 11:36:50 +0200

    Fixed issue #53

--- src/icons/view_boundary.py
+++ src/icons/view_boundary.py
@@ -1,4 +1,4 @@
-"""Icon for the following action: view drawing boundary"""
+"""Icon for the following action: view drawing boundary."""
 
 # This icon was converted from the Oxygen Icon Theme
 # Please see copyright for further informations
