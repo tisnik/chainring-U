9ebf7a7b8e4b843ac9a2d081010cecb8b6b49c95
Pavel Tisnovsky <ptisnovs@redhat.com>
2021-07-27 11:16:53 +0200

    Fixed issue #6

--- src/chainring.py
+++ src/chainring.py
@@ -28,7 +28,7 @@ from gui.dialogs.load_dialogs import LoadDialogs
 import sys
 
 configuration = Configuration()
-#configuration.write()
+# configuration.write()
 try:
     configuration.check_configuration()
 except Exception as e:
