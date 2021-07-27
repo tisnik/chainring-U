546a27a26920d1fbaae041e170183f4d97bed06a
Pavel Tisnovsky <ptisnovs@redhat.com>
2021-07-27 11:54:00 +0200

    Fixed issue #58

--- src/icons/insert_object.py
+++ src/icons/insert_object.py
@@ -1,4 +1,4 @@
-"""Icon for the following action: insert object"""
+"""Icon for the following action: insert object."""
 
 # This icon was converted from the Oxygen Icon Theme
 # Please see copyright for further informations
