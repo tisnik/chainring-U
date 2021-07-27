6aede3df2220280be0df3187d3562d081ce4038a
Pavel Tisnovsky <ptisnovs@redhat.com>
2021-07-27 11:54:25 +0200

    Fixed issue #56

--- src/icons/rooms_from_sap.py
+++ src/icons/rooms_from_sap.py
@@ -1,4 +1,4 @@
-"""Icon for the following action: rooms from SAP"""
+"""Icon for the following action: rooms from SAP."""
 
 # This icon was converted from the Oxygen Icon Theme
 # Please see copyright for further informations
