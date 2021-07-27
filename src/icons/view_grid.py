653d511a85a4bd4fc61dffb2cfc0777c781aece9
Pavel Tisnovsky <ptisnovs@redhat.com>
2021-07-27 11:54:34 +0200

    Fixed issue #55

--- src/icons/view_grid.py
+++ src/icons/view_grid.py
@@ -1,4 +1,4 @@
-"""Icon for the following action: view grid in drawing"""
+"""Icon for the following action: view grid in drawing."""
 
 # This icon was converted from the Oxygen Icon Theme
 # Please see copyright for further informations
