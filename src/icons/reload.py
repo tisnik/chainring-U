e357240df51c82e514451dbf97a4197046927555
Pavel Tisnovsky <ptisnovs@redhat.com>
2024-03-11 09:25:42 +0100

    Fixed docstring

--- src/icons/reload.py
+++ src/icons/reload.py
@@ -1,4 +1,4 @@
-"""Icon for the following action: reload drawing"""
+"""Icon for the following action: reload drawing."""
 
 # This icon was converted from the Oxygen Icon Theme
 # Please see copyright for further informations
