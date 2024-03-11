8221952cfd8937f73b151177fafa61bedfc7a035
Pavel Tisnovsky <ptisnovs@redhat.com>
2024-03-11 09:25:42 +0100

    Fixed docstring

--- src/icons/checkbox.py
+++ src/icons/checkbox.py
@@ -1,4 +1,4 @@
-"""Icon for the following action: classic checkbox"""
+"""Icon for the following action: classic checkbox."""
 
 # This icon was converted from the Oxygen Icon Theme
 # Please see copyright for further informations
