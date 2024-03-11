c15b5c6f09c9de23d32778bc94c4a4b00c8b1461
Pavel Tisnovsky <ptisnovs@redhat.com>
2024-03-11 09:25:42 +0100

    Fixed docstring

--- src/icons/help_about.py
+++ src/icons/help_about.py
@@ -1,4 +1,4 @@
-"""Icon for the following action: help/about"""
+"""Icon for the following action: help/about."""
 
 # This icon was converted from the Oxygen Icon Theme
 # Please see copyright for further informations
