34eb5d951e592fade6ee4c11d653e6f2208c2ff7
Pavel Tisnovsky <ptisnovs@redhat.com>
2024-03-11 09:25:42 +0100

    Fixed docstring

--- src/icons/system_run.py
+++ src/icons/system_run.py
@@ -1,4 +1,4 @@
-"""Icon for the following action: run something"""
+"""Icon for the following action: run something."""
 
 # This icon was converted from the Oxygen Icon Theme
 # Please see copyright for further informations
