c00d12b74f8ef82c070e489d592a9e2bf8dca647
Pavel Tisnovsky <ptisnovs@redhat.com>
2024-03-19 07:40:11 +0100

    Reformatted by Black

--- src/exporters/room_exporter.py
+++ src/exporters/room_exporter.py
@@ -47,8 +47,11 @@ class RoomExporter:
         vertexes = room["polygon"]
         # export only room with polygon
         if vertexes is not None:
-            fout.write("R {id} {vertex_count}".format(id=room["room_id"],
-                                                      vertex_count=len(vertexes)))
+            fout.write(
+                "R {id} {vertex_count}".format(
+                    id=room["room_id"], vertex_count=len(vertexes)
+                )
+            )
             for vertex in vertexes:
                 fout.write(" {x} {y}".format(x=vertex[0], y=vertex[1]))
         # room without polygon needs to have zero vertexes specified
