529bbbf2a69fa1ae38bdf3aacaea8f8d49e920a8
Pavel Tisnovsky <ptisnovs@redhat.com>
2024-03-19 07:40:11 +0100

    Reformatted by Black

--- src/gui/palette.py
+++ src/gui/palette.py
@@ -25,8 +25,7 @@ class Palette(tkinter.LabelFrame):
 
         self.listbox = tkinter.Listbox(self)
         self.listbox.pack(fill=tkinter.BOTH, expand=True)
-        self.group = tkinter.LabelFrame(self, text="Vybraná místnost",
-                                        padx=5, pady=5)
+        self.group = tkinter.LabelFrame(self, text="Vybraná místnost", padx=5, pady=5)
 
         self.label_id_name = tkinter.Label(self.group, text="ID")
         self.label_id_name.grid(column=1, row=1, sticky="W")
@@ -41,26 +40,39 @@ class Palette(tkinter.LabelFrame):
         self.label_contour = tkinter.Label(self.group, text="Obrys")
         self.label_contour.grid(column=1, row=4, sticky="W")
 
-        self.button1 = tkinter.Button(self.group, text="Smazat místnost ze seznamu",
-                                      compound="left",
-                                      command=self.delete_room_command,
-                                      image=main_window.icons.edit_delete_shred_icon,
-                                      width=200)
-        self.button2 = tkinter.Button(self.group, text="Vymazat",
-                                      compound="left",
-                                      command=self.delete_room_polygon_command,
-                                      image=main_window.icons.edit_delete_icon,
-                                      height=28)
-        self.button3 = tkinter.Button(self.group, text="Překreslit",
-                                      compound="left",
-                                      command=self.redraw_room_polygon_command,
-                                      image=main_window.icons.edit_redo_icon,
-                                      height=28)
-        self.button4 = tkinter.Button(self.group, text="Vybrat polyčáru",
-                                      compound="left",
-                                      command=self.select_polygon_for_room,
-                                      image=main_window.icons.rectangle,
-                                      width=200, height=28)
+        self.button1 = tkinter.Button(
+            self.group,
+            text="Smazat místnost ze seznamu",
+            compound="left",
+            command=self.delete_room_command,
+            image=main_window.icons.edit_delete_shred_icon,
+            width=200,
+        )
+        self.button2 = tkinter.Button(
+            self.group,
+            text="Vymazat",
+            compound="left",
+            command=self.delete_room_polygon_command,
+            image=main_window.icons.edit_delete_icon,
+            height=28,
+        )
+        self.button3 = tkinter.Button(
+            self.group,
+            text="Překreslit",
+            compound="left",
+            command=self.redraw_room_polygon_command,
+            image=main_window.icons.edit_redo_icon,
+            height=28,
+        )
+        self.button4 = tkinter.Button(
+            self.group,
+            text="Vybrat polyčáru",
+            compound="left",
+            command=self.select_polygon_for_room,
+            image=main_window.icons.rectangle,
+            width=200,
+            height=28,
+        )
         self.button1.grid(column=1, row=3, sticky="WE", columnspan=2)
         self.button2.grid(column=1, row=5, sticky="WE")
         self.button3.grid(column=2, row=5, sticky="WE")
