ecd38ab63d081fa2fd4ae3bb14df056e9cc71991
Pavel Tisnovsky <ptisnovs@redhat.com>
2024-03-19 07:40:11 +0100

    Reformatted by Black

--- src/importers/dxf_reader_state.py
+++ src/importers/dxf_reader_state.py
@@ -18,16 +18,16 @@ from enum import Enum
 class DxfReaderState(Enum):
     """State of the DXF reader finite automata."""
 
-    BEGINNING = 1,
-    BEGINNING_SECTION = 2,
-    SECTION_HEADER = 3,
-    SECTION_TABLES = 4,
-    SECTION_BLOCKS = 5,
-    SECTION_BLOCK = 6,
-    SECTION_BLOCK_ENTITY = 7,
-    SECTION_ENTITIES = 8,
-    SECTION_OBJECTS = 9,
-    SECTION_CLASSES = 10,
-    ENTITY = 11,
-    LINE_IN_BLOCK = 12,
+    BEGINNING = (1,)
+    BEGINNING_SECTION = (2,)
+    SECTION_HEADER = (3,)
+    SECTION_TABLES = (4,)
+    SECTION_BLOCKS = (5,)
+    SECTION_BLOCK = (6,)
+    SECTION_BLOCK_ENTITY = (7,)
+    SECTION_ENTITIES = (8,)
+    SECTION_OBJECTS = (9,)
+    SECTION_CLASSES = (10,)
+    ENTITY = (11,)
+    LINE_IN_BLOCK = (12,)
     EOF = 13
