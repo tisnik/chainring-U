ca6fa2ba1b33caf4bb9788518e254c45bcbd4f09
Pavel Tisnovsky <ptisnovs@redhat.com>
2024-03-27 13:15:52 +0100

    Added type hints

--- src/geometry/utils.py
+++ src/geometry/utils.py
@@ -17,6 +17,6 @@ class GeometryUtils:
     """Class with various geometry related utility functions."""
 
     @staticmethod
-    def square_length(x1, y1, x2, y2):
+    def square_length(x1: float, y1: float, x2: float, y2: float) -> float:
         """Compute square length."""
         return (x1 - x2) ** 2 + (y1 - y2) ** 2
