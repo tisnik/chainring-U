60767e7f4f917d43e94b2831d546c94ce717e7e5
Pavel Tisnovsky <ptisnovs@redhat.com>
2024-03-27 13:15:52 +0100

    Added type hints

--- src/geometry/rescaler.py
+++ src/geometry/rescaler.py
@@ -1,5 +1,9 @@
 """Class to recompute scale from canvas size and computed bounds."""
 
+from geometry.bounds import Bounds
+from gui.canvas import Canvas
+from typing import Tuple
+
 #
 #  (C) Copyright 2017, 2018  Pavel Tisnovsky
 #
@@ -17,7 +21,9 @@ class Rescaler:
     """Class to recompute scale from canvas size and computed bounds."""
 
     @staticmethod
-    def compute_scale(bounds, width, height):
+    def compute_scale(
+        bounds: Bounds, width: int, height: int
+    ) -> Tuple[float, float, float]:
         """Compute scale from given bounds, width, and height."""
         xdist = bounds.xmax - bounds.xmin
         ydist = bounds.ymax - bounds.ymin
@@ -27,7 +33,9 @@ class Rescaler:
         return -bounds.xmin, -bounds.ymin, scale
 
     @staticmethod
-    def compute_scale_for_canvas(bounds, canvas):
+    def compute_scale_for_canvas(
+        bounds: Bounds, canvas: Canvas
+    ) -> Tuple[float, float, float]:
         """Compute scale from given bounds and canvas (with width+height)."""
         canvas_width = canvas.winfo_reqwidth()
         canvas_height = canvas.winfo_reqheight()
